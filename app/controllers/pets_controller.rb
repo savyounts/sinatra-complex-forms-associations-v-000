bc717d0d434c68cba1c66f73ed7dede44916cc3c
Savannah Younts <savyounts@gmail.com>
2018-09-11 18:49:28 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,9 +13,9 @@ class PetsController < ApplicationController
   post '/pets' do
     binding.pry
     @pet = Pet.create(params)
-    # if !params[:owner_name].empty?
-    #   @pet.owner = Owner.create(name: params[:owner_name])
-    # end
+    if !params[:owner_name].empty?
+      @pet.owner = Owner.create(name: params[:owner_name])
+    end
     redirect to "pets/#{@pet.id}"
   end
 
