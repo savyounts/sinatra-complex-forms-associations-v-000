c32bad25c7b4270eeb766377aaf5369beef4819e
Savannah Younts <savyounts@gmail.com>
2018-09-11 19:21:32 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -34,6 +34,10 @@ class PetsController < ApplicationController
   patch '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update
+    if !params[:owner_name].empty?
+      @owner =Owner.create(name: params[:owner_name])
+      @pet = Pet.create(name: params[:pet][:name], owner_id: @owner.id)
+    end
     redirect to "pets/#{@pet.id}"
   end
 
